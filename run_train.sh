08e98b2629231f07e265c565366eef5425ed8c54
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2024-04-05 01:02:30 -0400

    Add new training scripts

---
+++ run_train.sh
@@ -0,0 +1,15 @@
+python design_baselines/diff/trainer.py --config configs/score_diffusion.cfg --seed 0 --use_gpu --mode 'train'\
+    --task ant \
+    --top_candidates_size 5000
+    
+python design_baselines/diff/trainer.py --config configs/score_diffusion.cfg --seed 1 --use_gpu --mode 'train'\
+    --task ant \
+    --top_candidates_size 5000
+    
+python design_baselines/diff/trainer.py --config configs/score_diffusion.cfg --seed 2 --use_gpu --mode 'train'\
+    --task ant \
+    --top_candidates_size 5000
+    
+python design_baselines/diff/trainer.py --config configs/score_diffusion.cfg --seed 3 --use_gpu --mode 'train'\
+    --task ant \
+    --top_candidates_size 5000
